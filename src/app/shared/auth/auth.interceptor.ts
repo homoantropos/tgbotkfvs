7e377f41479c263f722f9883a095c73147b35030
Sergii Antropov <homoantropos@gmail.com>
2022-04-04 16:16:08 +0300

    authentication layout added

---
+++ src/app/shared/auth/auth.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {AuthService} from "./auth.service";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(
+    private auth: AuthService
+  ) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.auth.isAuthenticated()) {
+      const token = this.auth.token;
+      if(token !== null)
+        req = req.clone({
+          setHeaders: {
+            Authorization: token
+          }
+        });
+    }
+    return next.handle(req);
+  }
+}
+
