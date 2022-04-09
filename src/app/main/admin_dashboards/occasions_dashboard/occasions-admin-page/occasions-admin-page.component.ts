93eaa45d4994c4e44d758d4f82c8a6b1e997464a
Segii Antropov <homoantropos@gmail.com>
2022-04-09 11:51:08 +0300

    occasion layout added

--- src/app/main/admin_dashboards/occasions_dashboard/occasions-admin-page/occasions-admin-page.component.ts
+++ src/app/main/admin_dashboards/occasions_dashboard/occasions-admin-page/occasions-admin-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Occasion} from "../../../../shared/interfaces";
 import {OccasionService} from "../../../../shared/services/occasion.service";
 import {AlertService} from "../../../../shared/services/alert.-service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-occasions-admin-page',
@@ -12,10 +13,17 @@ export class OccasionsAdminPageComponent implements OnInit {
 
   occasions: Array<Occasion>;
 
+  showButton = true;
+
+  searchOption = true;
   searchValue = '';
   searchField = ['name'];
 
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
     private occasionService: OccasionService,
     private alert: AlertService
   ) { }
@@ -28,6 +36,10 @@ export class OccasionsAdminPageComponent implements OnInit {
         }, error => this.alert.danger(
           error.erro.message ? error.erro.message : error)
       )
+  }
+
+  goToOccasionEditor(): void {
+
   }
 
 }
