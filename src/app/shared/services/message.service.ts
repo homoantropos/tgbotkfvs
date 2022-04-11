363412ee1520036005e6b82d1a7f2cc3cc9f0ec4
Sergii Antropov <homoantropos@gmail.com>
2022-04-11 15:43:50 +0300

    messanger component added

--- src/app/shared/services/message.service.ts
+++ src/app/shared/services/message.service.ts
@@ -9,6 +9,7 @@ import {environment} from "../../../environments/environment";
 export class MessageService {
 
   recipients: Array<number> = [];
+
   constructor(
     private http: HttpClient
   ) { }
