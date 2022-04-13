cf39b8f144eef8c7b0945308965cbd3546322b94
Sergii Antropov <homoantropos@gmail.com>
2022-04-13 14:29:11 +0300

    messanger component added

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const app = express();
 
 app.use(compression());
 
-app.use(express.static(path.join(__dirname, 'dist/selectsfactory')));
+app.use(express.static(path.join(__dirname, 'dist/tgbotkfvs')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/selectsfactory/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/tgbotkfvs/index.html'));
 });
 
 const port = process.env.PORT || 3000;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  backURI: 'https://kfvstgbot.herokuapp.com/api'
 };
