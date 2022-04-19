5429a8ee8c1a8b1df29b3966a0ae259e8ffc8374
Sergii Antropov <homoantropos@gmail.com>
2022-04-19 12:28:23 +0300

    message component added with functions send media

--- src/app/shared/services/message.service.ts
+++ src/app/shared/services/message.service.ts
@@ -18,4 +18,42 @@ export class MessageService {
   send(fd: FormData, method: string): Observable<any> {
     return this.http.post<any>(`https://api.telegram.org/bot${environment.bot_token}/${method}`, fd);
   }
+
+  getDefaultText(): string {
+    return `<b id="title"></b>
+<i title="description"></i>
+<b title="dates"></b><i></i>
+<b title="place"></b><i></i>
+<b title="organizers"></b><i></i>
+
+<a title="" href="">докладніше</a>
+`
+  }
+
+  getDefaultReplyMarkup(): any {
+    return {inline_keyboard:[[{text: '', url: ''}]]}
+  }
+
+  getMediaNameForMethod(method: string): string {
+    switch (method) {
+      case('sendMessage') :
+        return 'text';
+      case('sendPhoto') :
+        return 'photo';
+      case('sendVideo') :
+        return 'video';
+      case('sendDocument') :
+        return 'document';
+      case('sendAnimation') :
+        return 'animation';
+      case('sendAudio') :
+        return 'audio';
+      case('sendVoice') :
+        return 'voice';
+      case('sendVideoNote') :
+        return 'video_note';
+      default :
+        return 'media';
+    }
+  }
 }
